b902a94649be58c1f911cfb96c2c2abd08baf5fe
大塚 泰司 <yasushi-ohtsuka@nifty.ne.jp>
2017-11-05 19:42:30 +0900

    REST Client

--- SampleJavaEE7/src/test/client/SampleRestClient.java
+++ SampleJavaEE7/src/test/client/SampleRestClient.java
@@ -3,7 +3,6 @@ package test.client;
 import java.io.IOException;
 import java.util.Map;
 
-import javax.ws.rs.BadRequestException;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
@@ -12,45 +11,39 @@ import javax.ws.rs.core.Form;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedHashMap;
 import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.Response;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class SampleRestClient {
 
-	private static Client client = ClientBuilder.newClient();
-
 	public static void main(String[] args) {
+
+		Client client = ClientBuilder.newClient();
 
 		// GET
 		WebTarget target = client.target("http://localhost:8080")
 				.path("/oauth/access_token")
 				.queryParam("token", "shortAccessToken");
-		String result = "";
-		try {
-			result = target.request().get(String.class);
-		} catch (BadRequestException e) {
-			System.out.println("response=" + e.getResponse().readEntity(String.class));
-			e.printStackTrace();
-		}
-		System.out.println("result=" + result);
+		Response res = null;
+
+		res = target.request().get();
+
+		System.out.println("result=" + res.getStatus());
+		System.out.println("resultData=" + res.readEntity(String.class));
 
 		// POST
 		Entity<Form> entity = Entity.entity(new Form()
 				.param("name", "name")
 				.param("subtype", "CUSTOM"),
 				MediaType.APPLICATION_FORM_URLENCODED_TYPE);
-
-		try {
-		    result = client.target("http://localhost:8080")
+		res= client.target("http://localhost:8080")
 		        .path("/path_to_post_method")
-		        .request()
-		        .post(entity, String.class);
-		} catch (BadRequestException e) {
-			System.out.println("response=" + e.getResponse().readEntity(String.class));
-			e.printStackTrace();
-		}
-		System.out.println("result=" + result);
+		        .request(MediaType.APPLICATION_JSON)
+		        .post(entity);
+		System.out.println("result=" + res.getStatus());
+		System.out.println("resultData=" + res.readEntity(String.class));
 
 		// HTTPヘッダ
 		MultivaluedMap<String, Object> headers = new MultivaluedHashMap<>();
@@ -61,7 +54,7 @@ public class SampleRestClient {
 		System.out.println("jsonString=" + jsonString);
 
 		// JSONパース
-		result = client.target("http://localhost:8080")
+		String result = client.target("http://localhost:8080")
 			    .path("/path_to_post_method")
 			    .request()
 			    .post(entity, String.class);
