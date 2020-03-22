37cc39fc58a8a6bb1a7e69b723c513cf9794b35d
大塚 泰司 <yasushi-ohtsuka@nifty.ne.jp>
2020-03-22 15:45:15 +0900

    ファイル入出力

---
+++ FileIO/src/test/BinaryFileInputNio.java
@@ -0,0 +1,39 @@
+package test;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * バイナリーファイルを読み込む
+ */
+public class BinaryFileInputNio {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		// 読み込みファイルの名前
+		Path path = Paths.get("C:/dk", "viewcount.xlsx");
+
+		try {
+			byte[] data = Files.readAllBytes(path);
+			String hex;
+			for (int i = 0; i < data.length; i++) {
+				hex = String.format("%1$x", data[i]);
+				hex = hex.length() == 1 ? "0" + hex : hex;
+				System.out.print(hex + " ");
+			}
+			System.out.println();
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
